334fc3fdb0401e325ecd0c50f1aa6658d1395849
Carolina <carolti2013@gmail.com>
2023-12-21 19:56:55 -0300

    criando o cadastro de produtos

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // importar modulo express
 const express = require('express')
-import { engine } from 'express-handlebars';
+//importa modulo express-handlebars
+const { engine } = require('express-handlebars');
+//importar modulo de upload
+const fileupload = require('express-fileupload')
 
 // importar modulo postgresql
 const Pool = require('pg').Pool
@@ -9,6 +12,10 @@ const bodyParser = require('body-parser')
 
 //App
 const app = express()
+
+//habilitando o upload de arquivos
+app.use(fileupload())
+
 //configuração do handlebars
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
@@ -32,11 +39,25 @@ app.use(
     extended: true,
   })
 )
+//adicionar bootstrap
+app.use('/bootstrap',express.static('./node_modules/bootstrap/dist'))
 
-//rota Hello world
-app.get('', (req, res) => {
-    res.send('Hello World')
+//adicionar css
+app.use('/css', express.static('./css'))
+
+//rota principal
+app.get('/', (req, res) => {
+    res.render('formulario')
 })
+//rota de cadastro
+app.post('/cadastrar', (req, res) => {
+    console.log(req.body)
+    res.end()
+})
+//rota de upload
+app.post('/upload', (req, res)=> {
+  console.log(req.files.imagem.name); // the uploaded file object
+});
 //Servidor
 app.listen(PORT, () => {
     console.log(`Rodando na porta http://localhost:${PORT}`);
