3ad053f855282cfaeafa3867b40e1482499faa78
carolina <carolti2013@gmail.com>
2023-12-21 15:53:42 -0300

    criação handlebars

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 // importar modulo express
 const express = require('express')
+import { engine } from 'express-handlebars';
+
+// importar modulo postgresql
+const Pool = require('pg').Pool
+
+const bodyParser = require('body-parser')
+
 //App
 const app = express()
+//configuração do handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+//configuração do banco postgresql
+const pool = new Pool ({
+    host:'localhost',
+    user:'postgres',
+    password:'postgres',
+    database:'projeto',
+    port:5432
+})
+
 
 const PORT = 3000;
 
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 //rota Hello world
 app.get('', (req, res) => {
