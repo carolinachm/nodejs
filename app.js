7cf77e50a7c7072f5251d35787a1ef7696d29f91
carolina <carolti2013@gmail.com>
2023-12-28 20:36:10 -0300

    refatorando o codigo e atualizando o projeto

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const fileupload = require('express-fileupload')
 //file-system(manipular arquivos)
 const fs = require('fs');
 
-const pool = require('./db/configDB').pool
+const pool = require('./src/db/configDB').pool
+const db = require('./src/db/produtoQueries')
 
 //App
 const app = express()
@@ -49,153 +50,12 @@ app.use('/imagens', express.static('./imagens'))
 //     res.render('formulario')
 // })
 
-//rota para retornar todos os produtos
-app.get('/', (req, res) => {
-    //SQL
-    let sql = (`SELECT * FROM produtos`)
-    //executar o sql
-    pool.query(sql, function (erro, retorno) {
-        res.render('formulario', { produtos: retorno.rows })
-    });
-});
-//rota principal contendo a situação
-app.get('/:situacao', (req, res) => {
-    //SQL
-    let sql = (`SELECT * FROM produtos`)
-    //executar o sql
-    pool.query(sql, function (erro, retorno) {
-        res.render('formulario', { produtos: retorno.rows, situacao: req.params.situacao })
-    });
-});
-
-//rota de cadastro
-app.post('/cadastrar', (req, res) => {
-    try {
-        //Obter os dados que serão utilizados para o cadastro
-        let nome = req.body.nome;
-        let descricao = req.body.descricao;
-        let valor = req.body.valor;
-        let imagem = req.files.imagem.name;
-
-        //validar o nome do produto e valor
-        if (nome == '' || descricao == '' || valor == '' || isNaN(valor)) {
-            res.redirect('/falhaCadastro')
-
-        } else {
-            //SQL
-            let sql = `INSERT INTO produtos(nome, descricao, valor, imagem) VALUES ('${nome}', '${descricao}', ${valor}, '${imagem}')`;
-
-            //executar o sql
-            pool.query(sql, function (erro, retorno) {
-                if (erro) throw erro;
-                //caso ocorra o cadastro
-                req.files.imagem.mv(__dirname + '/imagens/' + req.files.imagem.name)
-                console.log(retorno)
-
-            });
-
-            //Retorna para tela principal
-            res.redirect('/OKCadastro')
-
-        }
-
-    } catch (erro) {
-        res.redirect('/falhaCadastro')
-
-    }
-})
-//rota para remover produtos
-app.get('/remover/:codigo&:imagem', (req, res) => {
-    //tratativa de exceção
-    try {
-        // Sql
-        let sql = `DELETE FROM produtos WHERE codigo = ${req.params.codigo}`
-        //Executando sql
-        pool.query(sql, function (erro, retorno) {
-            //Caso false o comando sql
-            if (erro) throw erro
-            //Caso o comando sql funcione
-            fs.unlink(__dirname + '/imagens/' + req.params.imagem, (erro_imagem) => {
-                console.log('Falha ao remover a imagem')
-            })
-        })
-        //Redirecionamento
-        res.redirect('/OKRemover')
-
-    } catch (erro) {
-        res.redirect('/falha ao remover')
-    }
-})
-
-//rota para editar produto
-app.get('/formularioEditar/:codigo', function (req, res) {
-    //SQL
-    let sql = `SELECT * FROM produtos WHERE codigo = ${req.params.codigo}`;
-
-    //Executar o sql
-    pool.query(sql, function (erro, retorno) {
-        //caso aja erro
-        if (erro) throw erro
-        //Caso de certo
-        res.render('formularioEditar', { produto: retorno[0] })
-    })
-})
-
-
-// *************** FUNÇÃO PARA EDITAR OS PRODUTOS
-// Rota para editar produtos
-app.post('/editar', function (req, res) {
-
-    // Obter os dados do formulário
-    let nome = req.body.nome;
-    let descricao = req.body.descricao;
-    let valor = req.body.valor;
-    let codigo = req.body.codigo;
-    let nomeImagem = req.body.nomeImagem;
-
-    //validar nome do produto e valor
-    if (nome == '' || descricao == '' || valor == '' || isNaN(valor)) {
-        res.redirect('/falhaCadastro')
-
-    } else {
-        //Definir o tipo de edição
-        try {
-            //objeto de imagem
-            let imagem = req.files.imagem
-            // SQL
-            let sql = `UPDATE produtos SET nome='${nome}',descricao='${descricao}', valor=${valor}, imagem='${imagem}' 
-        WHERE codigo=${codigo}`;
-            //executar comando sql
-            pool.query(sql, function (erro, retorno) {
-                //caso falhe o comando sql
-                if (erro) throw erro
-                //remover imagem antiga
-                fs.unlink(__dirname + '/imagens' + nomeImagem, (erro_imagem) => {
-                    console.log('Falha ao remover a imagem')
-                })
-                //Cadastrar nova imagem
-                imagem.mv(__dirname + '/imagens/' + imagem.name);
-            })
-        } catch (erro) {
-            //SQL
-            let sql = `UPDATE produtos SET nome='${nome}',descricao='${descricao}', valor=${valor} 
-        WHERE codigo=${codigo}`;
-
-            //Executar comando sql
-            pool.query(sql, function (erro, retorno) {
-                //Caso falhe o comando sql
-                if (erro) throw erro;
-            })
-
-        }
-
-        // Redirecionamento
-        res.redirect('/OKEdicao');
-    }
-
-
-});
-
+//rota de produtos
+app.get('/produtos', db.buscarTodosProdutos)
+app.get('/produtos/:id', db.buscarProdutosPorId)
+app.post('/produtos', db.cadastrarProdutos)
+app.put('/produtos/:id', db.atualizarProdutos)
+app.delete('/produtos/:id', db.removerProdutos)
 
 //Servidor
 app.listen(PORT, () => {
